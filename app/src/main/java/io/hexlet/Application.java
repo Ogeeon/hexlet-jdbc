89528ee3288df86402fa71b4e8afef254f3afd43
Alexey Bogatyrev <avbogatyrev@gmail.com>
2025-09-10 07:16:42 +0000

    Introduce prepared statements

--- app/src/main/java/io/hexlet/Application.java
+++ app/src/main/java/io/hexlet/Application.java
@@ -2,29 +2,59 @@ package io.hexlet;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Application {
     // Нужно указывать базовое исключение,
     // потому что выполнение запросов может привести к исключениям
     public static void main(String[] args) throws SQLException {
+        long termId;
         try (java.sql.Connection conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
             var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
             try (java.sql.Statement statement = conn.createStatement()) {
                 statement.execute(sql);
             }
-            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (java.sql.Statement statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
+            sql = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, "Sarah");
+                preparedStatement.setString(2, "333333333");
+                preparedStatement.executeUpdate();
+                var generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    System.out.println("Inserted with ID=" + generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("DB have not returned an id after saving the entity");
+                }
+                preparedStatement.setString(1, "John");
+                preparedStatement.setString(2, "222222");
+                preparedStatement.executeUpdate();
+                generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    System.out.println("Inserted with ID=" + generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("DB have not returned an id after saving the entity");
+                }
+                preparedStatement.setString(1, "T-1000");
+                preparedStatement.setString(2, "1111");
+                preparedStatement.executeUpdate();
+                generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    termId = generatedKeys.getLong(1);
+                    System.out.println("Inserted with ID=" + termId);
+                } else {
+                    throw new SQLException("DB have not returned an id after saving the entity");
+                }
+            }
+            sql = "DELETE FROM users WHERE id = ?";
+            try (var preparedStatement = conn.prepareStatement(sql)) {
+                preparedStatement.setLong(1, termId);
+                preparedStatement.executeUpdate();
             }
             var sql3 = "SELECT * FROM users";
-            // Здесь вы видите указатель на набор данных в памяти СУБД
             try (java.sql.Statement statement3 = conn.createStatement()) {
-                // Здесь вы видите указатель на набор данных в памяти СУБД
                 var resultSet = statement3.executeQuery(sql3);
-                // Набор данных — это итератор
-                // Мы перемещаемся по нему с помощью next() и каждый раз получаем новые значения
                 while (resultSet.next()) {
-                    System.out.println(resultSet.getString("username"));
+                    System.out.print(resultSet.getString("username") + " ");
                     System.out.println(resultSet.getString("phone"));
                 }
             }
